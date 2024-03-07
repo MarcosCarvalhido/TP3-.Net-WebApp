5b508c2a522a6e04615f5b74871eaf7341e44ad3
Marcos Carvalhido Rodrigues <marcoscrodrigues8@gmail.com>
2024-03-07 10:39:32 -0300

    Add project files.

---
+++ InfnetWebApp/Migrations/20240307130954_inicio.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InfnetWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class inicio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aluno",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Endereco = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nascimento = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aluno", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aluno");
+        }
+    }
+}
---
+++ InfnetWebApp/Models/Aluno.cs
@@ -0,0 +1,26 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace InfnetWebApp.Models
+{
+    public class Aluno
+    {
+        [Required(ErrorMessage ="Id Obrigatorio")]
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Nome Obrigatorio")]
+        [Display(Name = "Nome do Aluno")]
+        [StringLength(50, MinimumLength = 4, ErrorMessage ="Nome deve ter entre 5 e 50 Caracteries")]
+        public string Nome { get; set; }
+
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+
+        [DataType(DataType.EmailAddress,ErrorMessage = "Prencha apenas com Emails!")]
+        public string Email { get; set; }
+
+        [Display(Name = "Data de Nascimento")]
+        [DataType(DataType.Date,ErrorMessage ="Prencha apenas com datas!")]
+        public string Nascimento { get; set; }
+    }
+}
